01076119af6f15becc7ec3b55f8348c0eb5f479b
shishantkr1408 <shishantkr2003@gmail.com>
2025-08-22 15:24:02 +0530

    Added basename in browser-router

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,46 @@ import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Paste from "./components/Paste"
 import ViewPaste from "./components/ViewPaste"
-const router = createBrowserRouter([
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <div>
+          <Navbar />
+          <Home />
+        </div>
+      ),
+    },
+    {
+      path: "/pastes",
+      element: (
+        <div>
+          <Navbar />
+          <Paste />
+        </div>
+      ),
+    },
+    {
+      path: "/pastes/:id",
+      element: (
+        <div>
+          <Navbar />
+          <ViewPaste />
+        </div>
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <div>
-        <Navbar />
-        <Home />
-      </div>
-    ),
-  },
-  {
-    path: "/pastes",
-    element: (
-      <div>
-        <Navbar />
-        <Paste />
-      </div>
-    ),
-  },
-  {
-    path: "/pastes/:id",
-    element: (
-      <div>
-        <Navbar />
-        <ViewPaste />
-      </div>
-    ),
-  },
-]);
+    basename: "/Notes-saver-Application/"
+  }
+);
 
 function App() {
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
